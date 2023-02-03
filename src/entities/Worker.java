0838da3dbed567e7097bc83f09f655f41e087ab9
Marlu.Patrocinio <marlupatrocinio01@gmail.com>
2023-02-03 14:56:36 -0300

    first commit

---
+++ src/entities/Worker.java
@@ -0,0 +1,87 @@
+package entities;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
+import entities.enums.WorkerLevel;
+
+public class Worker {
+	private String name;
+	private WorkerLevel level;
+	private Double baseSallary;
+
+	private Departament departament;
+	private List<HourContract> contracts = new ArrayList<>(); //multiple contracts needs to be set as list.
+
+	public Worker() {
+
+	}
+
+	public Worker(String name, WorkerLevel level, Double baseSallary, Departament departament) {
+
+		this.name = name;
+		this.level = level;
+		this.baseSallary = baseSallary;
+		this.departament = departament;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public WorkerLevel getLevel() {
+		return level;
+	}
+
+	public void setLevel(WorkerLevel level) {
+		this.level = level;
+	}
+
+	public Double getBaseSallary() {
+		return baseSallary;
+	}
+
+	public void setBaseSallary(Double baseSallary) {
+		this.baseSallary = baseSallary;
+	}
+
+	public Departament getDepartament() {
+		return departament;
+	}
+
+	public void setDepartament(Departament departament) {
+		this.departament = departament;
+	}
+
+	public List<HourContract> getContracts() {
+		return contracts;
+	}
+
+	public void addContract(HourContract contract) {
+		contracts.add(contract);
+	}
+
+	public void removeContract(HourContract contract) {
+		contracts.remove(contract);
+	}
+	
+	public Double income(int year, int month) {
+		double sum = baseSallary;
+		Calendar cal = Calendar.getInstance();
+		for (HourContract c : contracts) {
+			cal.setTime(c.getDate());
+			int c_year = cal.get(Calendar.YEAR);
+			int c_month = 1 + cal.get(Calendar.MONTH);
+			if (year == c_year && month == c_month) {
+				sum += c.totalValue();
+			}
+		}
+		return sum;
+	}
+	
+}
